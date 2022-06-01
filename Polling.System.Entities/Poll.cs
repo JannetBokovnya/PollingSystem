8a4df877ab88d6489a5b471c9a893345a77c65b9
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2022-06-01 18:16:24 +0300

    git start polling system

---
+++ Polling.System.Entities/Poll.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//голосование
+public class Poll
+{
+    //голосование без вопроса не может быть поэтому создаем конструктор
+    public Poll(string questionText, List<PollAnswer> answers)
+    {
+        QuestionText = questionText;
+        Answers = answers;
+    }
+    //в модель помещаем поведение объекта
+    //VoteTo сколько прибавляется готосов за тот или иной выбор
+    public void VoteTo(int id, int vote = 1)
+    {
+        var item = Answers?.SingleOrDefault(x => x.Id == id);
+        if (item == null)
+        {
+            return;
+        }
+        item.Votes += vote;
+
+        var totalVotes = Answers?.Sum(x => x.Votes) ?? 0;
+        //item.SetPercents(totalVotes);
+        Answers?.ForEach(x => x.SetPercents(totalVotes));
+    }
+
+    public string QuestionText { get; }
+    public List<PollAnswer>? Answers { get; }
+
+    //переопределяем строку (нужно для вывода в консоль для теста)
+    //перенесено в PollResults
+    //public override string ToString()
+    //{
+
+    //    var stringBuilder = new StringBuilder(QuestionText);
+    //    stringBuilder.AppendLine("-".PadLeft(50));
+    //    if (Answers != null && Answers.Any())
+    //    {
+    //        Answers.ForEach(x => stringBuilder.AppendLine(x.ToString()));
+    //    }
+    //    return stringBuilder.ToString();
+    //}
+}   
+
+//ответы на вопросы
+public class PollAnswer
+{
+    public PollAnswer(int id, string title)
+    {
+        Title = title;
+        Id = id;
+    }
+    public int Id { get; }
+    public string Title { get; }
+
+    //количество голосов
+    public int Votes { get; set; }
+
+    //проценты
+    public double Percents { get; set; }
+
+    //рассчитываем процент голосов
+    public void SetPercents(int totalVoites)
+    {
+        if (totalVoites > 0)
+        {
+            Percents = Votes * 100d / totalVoites;
+        }
+
+        
+    }
+    public override string ToString()
+    {
+        return $"* {Title} ({Votes} {Percents:F})";
+
+    }
+}
+
+
---
+++ PollingSystem.ConsoleClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+//вместо используем FluentInterface
+
+var builder = new PollBuilder("Как вам фильм Матрица?")
+    .AddAnswer(1, "Нормально")
+    .AddAnswer(2, "Не плохо")
+    .AddAnswer(3, "Отстой")
+    .AddAnswer(4, "Супер");
+
+
+var poll = builder.Build();
+
+poll.VoteTo(1);
+poll.VoteTo(1);
+poll.VoteTo(2);
+poll.VoteTo(3);
+poll.VoteTo(4);
+poll.VoteTo(4);
+poll.VoteTo(4);
+poll.VoteTo(4, 10);
+
+var result = builder.GetResults(poll);
+
+Console.WriteLine(result.GetView());
+
+
+
+
+//вместо используем FluentInterface
+//var poll = new Poll("Как вам фильм Матрица?")
+//{
+
+//    Answers=new List<PollAnswer>
+//    {
+//        new(1,"Нормально"),
+//        new(2,"Не плохо"),
+//        new(3,"Отстой"),
+//        new(4, "Супер")
+//    }
+
+//};
+////просто выбырием вариант ответа
+//poll.VoteTo(1);
+//poll.VoteTo(2);
+//poll.VoteTo(3);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4, 10);
+
+//Console.WriteLine(poll);
