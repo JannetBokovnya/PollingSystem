61faf549e7f0d29ca7307ff6a000719ce5235e01
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2022-06-04 23:00:16 +0300

    project structure, database+web

--- Polling.System.Entities/Poll.cs
+++ Polling.System.Entities/Poll.cs
@@ -1,86 +1,63 @@
-﻿using System;
+﻿
+using PollingSystem.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-
-//голосование
-public class Poll
+//это главная зборка на нее ссылается application(данные)
+//client ссылаетсы на application
+namespace PollingSystem.Entities
 {
-    //голосование без вопроса не может быть поэтому создаем конструктор
-    public Poll(string questionText, List<PollAnswer> answers)
+    //голосование
+    public class Poll : Identity
     {
-        QuestionText = questionText;
-        Answers = answers;
-    }
-    //в модель помещаем поведение объекта
-    //VoteTo сколько прибавляется готосов за тот или иной выбор
-    public void VoteTo(int id, int vote = 1)
-    {
-        var item = Answers?.SingleOrDefault(x => x.Id == id);
-        if (item == null)
+        //голосование без вопроса не может быть поэтому создаем конструктор
+        public Poll(string questionText, List<PollAnswer> answers):this(questionText)
         {
-            return;
-        }
-        item.Votes += vote;
-
-        var totalVotes = Answers?.Sum(x => x.Votes) ?? 0;
-        //item.SetPercents(totalVotes);
-        Answers?.ForEach(x => x.SetPercents(totalVotes));
-    }
-
-    public string QuestionText { get; }
-    public List<PollAnswer>? Answers { get; }
-
-    //переопределяем строку (нужно для вывода в консоль для теста)
-    //перенесено в PollResults
-    //public override string ToString()
-    //{
-
-    //    var stringBuilder = new StringBuilder(QuestionText);
-    //    stringBuilder.AppendLine("-".PadLeft(50));
-    //    if (Answers != null && Answers.Any())
-    //    {
-    //        Answers.ForEach(x => stringBuilder.AppendLine(x.ToString()));
-    //    }
-    //    return stringBuilder.ToString();
-    //}
-}   
-
-//ответы на вопросы
-public class PollAnswer
-{
-    public PollAnswer(int id, string title)
-    {
-        Title = title;
-        Id = id;
-    }
-    public int Id { get; }
-    public string Title { get; }
-
-    //количество голосов
-    public int Votes { get; set; }
-
-    //проценты
-    public double Percents { get; set; }
-
-    //рассчитываем процент голосов
-    public void SetPercents(int totalVoites)
-    {
-        if (totalVoites > 0)
-        {
-            Percents = Votes * 100d / totalVoites;
+            QuestionText = questionText;
+            Answers = answers;
         }
 
-        
-    }
-    public override string ToString()
-    {
-        return $"* {Title} ({Votes} {Percents:F})";
+        private Poll(string questionText)
+        {
+            QuestionText = questionText;
+        }
+        //в модель помещаем поведение объекта
+        //VoteTo сколько прибавляется готосов за тот или иной выбор
+        public void VoteTo(Guid id, int vote = 1)
+        {
+            var item = Answers?.SingleOrDefault(x => x.Id == id);
+            if (item == null)
+            {
+                return;
+            }
+            item.Votes += vote;
 
+            var totalVotes = Answers?.Sum(x => x.Votes) ?? 0;
+            //item.SetPercents(totalVotes);
+            Answers?.ForEach(x => x.SetPercents(totalVotes));
+        }
+
+        public string QuestionText { get; init; }
+        public List<PollAnswer>? Answers { get; init; }
+
+        //переопределяем строку (нужно для вывода в консоль для теста)
+        //перенесено в PollResults
+        //public override string ToString()
+        //{
+
+        //    var stringBuilder = new StringBuilder(QuestionText);
+        //    stringBuilder.AppendLine("-".PadLeft(50));
+        //    if (Answers != null && Answers.Any())
+        //    {
+        //        Answers.ForEach(x => stringBuilder.AppendLine(x.ToString()));
+        //    }
+        //    return stringBuilder.ToString();
+        //}
     }
 }
+
+
+
 
 
--- PollingSystem.ConsoleClient/Program.cs
+++ PollingSystem.ConsoleClient/Program.cs
@@ -1,26 +1,65 @@
-﻿using System;
+﻿using PollingSystem.Application;
+using System;
 
 using System.Collections.Generic;
 //вместо используем FluentInterface
 
 var builder = new PollBuilder("Как вам фильм Матрица?")
-    .AddAnswer(1, "Нормально")
-    .AddAnswer(2, "Не плохо")
-    .AddAnswer(3, "Отстой")
-    .AddAnswer(4, "Супер");
-
+    .AddAnswer(Guid.Parse("51611623-e72f-0488-4011-be4f13c8e936"), "Нормально")
+    .AddAnswer(Guid.Parse("b253b82c-90a1-2183-4053-48f910a49247"), "Не плохо")
+    .AddAnswer(Guid.Parse("50f0b6ee-ba6e-f988-4f3b-79c85308ed25"), "Отстой")
+    .AddAnswer(Guid.Parse("9ebb2234-75cb-eeb2-4fed-1774318d9ce8"), "Супер")
+    .AddAnswer(Guid.Parse("51611623-e72f-0488-4011-be4f13c8e936"), "Очень круто");
 
 var poll = builder.Build();
 
-poll.VoteTo(1);
-poll.VoteTo(1);
-poll.VoteTo(2);
-poll.VoteTo(3);
-poll.VoteTo(4);
-poll.VoteTo(4);
-poll.VoteTo(4);
-poll.VoteTo(4, 10);
+poll.VoteTo(Guid.Parse("50f0b6ee-ba6e-f988-4f3b-79c85308ed25"));
+poll.VoteTo(Guid.Parse("50f0b6ee-ba6e-f988-4f3b-79c85308ed25"));
+poll.VoteTo(Guid.Parse("b253b82c-90a1-2183-4053-48f910a49247"));
+poll.VoteTo(Guid.Parse("b253b82c-90a1-2183-4053-48f910a49247"));
+poll.VoteTo(Guid.Parse("51611623-e72f-0488-4011-be4f13c8e936"));
+poll.VoteTo(Guid.Parse("51611623-e72f-0488-4011-be4f13c8e936"));
+poll.VoteTo(Guid.Parse("b253b82c-90a1-2183-4053-48f910a49247"));
+poll.VoteTo(Guid.Parse("9ebb2234-75cb-eeb2-4fed-1774318d9ce8"));
+poll.VoteTo(Guid.Parse("9ebb2234-75cb-eeb2-4fed-1774318d9ce8"));
+poll.VoteTo(Guid.Parse("9ebb2234-75cb-eeb2-4fed-1774318d9ce8"));
+poll.VoteTo(Guid.Parse("9ebb2234-75cb-eeb2-4fed-1774318d9ce8"), 10);
 
+//var builder = new PollBuilder("Как вам фильм Матрица?")
+//    .AddAnswer(Guid.Parse("51611623-e72f-0488-4011-be4f13c8e936"), "Нормально")
+//    .AddAnswer(2, "Не плохо")
+//    .AddAnswer(3, "Отстой")
+//    .AddAnswer(4, "Супер");
+
+
+//var poll = builder.Build();
+
+//poll.VoteTo(1);
+//poll.VoteTo(1);
+//poll.VoteTo(2);
+//poll.VoteTo(3);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4);
+//poll.VoteTo(4, 10);
+
+//хотим поместить в базу данных
+using (var context=new ApplicationDbContext())
+{
+    //сделать конфигурацию в dbcontexte(в режиме памяти, т.к. пока там работаем)
+    context.Polls.Add(poll);
+    context.SaveChanges();
+
+}
+//проверка что записалось в базу
+using (var context = new ApplicationDbContext())
+{
+    foreach(var answer in context.Answers)
+    {
+        Console.WriteLine(answer.Title);
+    }
+
+}
 var result = builder.GetResults(poll);
 
 Console.WriteLine(result.GetView());
