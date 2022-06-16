1206fcc4ef27c1b1ac343c4bf03a00356f384ca4
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2022-06-16 22:09:17 +0300

    admin page with Cookie

---
+++ PollingSystem.Web/Pages/Admin/Logout.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PollingSystem.Web.Pages.Admin
+{
+    [Authorize]
+    public class LogoutModel : PageModel
+    {
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(ILogger<LogoutModel> logger) => _logger = logger;
+        public async Task<IActionResult> OnPost(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            await HttpContext.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            return RedirectToPage();
+
+        }
+    }
+}
